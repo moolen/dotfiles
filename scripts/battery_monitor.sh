2da16be56425215356b0adf12c21d52ef3655ab7
Moritz Johner <beller.moritz@googlemail.com>
2018-10-31 21:11:29 +0100

    add battery monitor

---
+++ scripts/battery_monitor.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+
+while true
+do
+    state=` acpi | head -1 | awk '{print $3}'`
+    charge=`acpi | head -1 | grep -P -o '[0-9]+(?=%)'`
+
+    if [ $state = "Discharging," ] && [ $charge -le 20 ]; then
+        notify-send "Battery low, ${charge}% remaining"
+    fi
+    sleep 120
+done
