b7cfdd73cea8430e50d3dfd1e0837e3ca019d88f
Moritz Johner <beller.moritz@googlemail.com>
2020-08-21 15:57:01 +0200

    feat: add force-delete-namespace fn

--- zsh/keybindings.sh
+++ zsh/keybindings.sh
@@ -145,3 +145,10 @@ function ax() {
     ansible -i "$INVENTORY" -a "$CMD" "$HOSTS"
 }
 
+function fdl() {
+  NS=$1
+  kubectl get namespace "$1" -o json \
+  | tr -d "\n" | sed "s/\"finalizers\": \[[^]]\+\]/\"finalizers\": []/" \
+  | kubectl replace --raw /api/v1/namespaces/$1/finalize -f -
+}
+
